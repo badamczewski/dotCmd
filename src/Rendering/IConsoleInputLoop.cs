0e5f053fb908c8dcd0842f5ce7f0b7126adac74f
badamczewski <bartoszadamczewski@gmail.com>
2016-11-20 10:44:16 +0100

    Input Loop Interface

---
+++ src/Rendering/IConsoleInputLoop.cs
@@ -0,0 +1,20 @@
+﻿using dotCmd.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotCmd.Rendering
+{
+    /// <summary>
+    /// Represents an input loop, that reads content from the input buffer.
+    /// </summary>
+    public interface IConsoleInputLoop
+    {
+        /// <summary>
+        /// Reads the input from the input buffer.
+        /// </summary>
+        ReadRef ReadInput(InputOptions options);
+    }
+}
